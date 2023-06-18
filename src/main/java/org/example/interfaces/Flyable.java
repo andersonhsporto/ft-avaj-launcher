65f0c2d65fd7ced7c1f50365d7cec440d520f035
Anderson Porto <anderson.higo2@gmail.com>
2023-06-17 23:30:50 -0300

    refactor: member functions in interface

--- src/main/java/org/example/interfaces/Flyable.java
+++ src/main/java/org/example/interfaces/Flyable.java
@@ -1,10 +1,16 @@
 package org.example.interfaces;
 
 import org.example.tower.WeatherTower;
+import org.example.types.Coordinates;
 
 public interface Flyable {
     public void updateConditions();
 
     public void registerTower(WeatherTower weatherTower);
-	
+
+    public String getFlyableName();
+
+    public Coordinates getFlyableCoordinates();
+
+    public String getFlyableLog();
 }
